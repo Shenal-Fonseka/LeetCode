fd5790fc8f21956e007f48242dccf694a1c31906
Shenal Fonseka <146927943+Shenal-Fonseka@users.noreply.github.com>
2023-10-06 09:36:04 +0530

    Time: 66 ms (6.74%), Space: 41.5 MB (84.08%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,12 @@
+
+var filter = function(arr, fn) {
+    let filteredArr = []
+    
+    for (let i=0; i < arr.length; i++){
+        if(fn(arr[i],i)){
+            filteredArr.push(arr[i])
+        }
+    }
+    return filteredArr
+};
+
