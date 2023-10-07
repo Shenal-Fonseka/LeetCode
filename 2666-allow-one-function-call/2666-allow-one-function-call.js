4a68c7aa45e26764f90659afb429838e6d6e3f8e
Shenal Fonseka <146927943+Shenal-Fonseka@users.noreply.github.com>
2023-10-07 10:42:56 +0530

    Time: 52 ms (54.54%), Space: 41.7 MB (62.41%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,18 @@
+var once = function(fn) {
+    let runOnce = false
+	return function(...args){
+        if (runOnce === false){
+            runOnce = true
+            return fn(...args)
+        }  
+      
+    }
+}
+
+
+//  let fn = (a,b,c) => (a + b + c)
+//  let onceFn = once(fn)
+ 
+//  onceFn(1,2,3); // 6
+//  onceFn(2,3,6); // returns undefined without calling fn
+ 
