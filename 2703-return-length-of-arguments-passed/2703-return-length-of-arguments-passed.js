f49412c3d805a06b86aced2ef663bc29075f27fc
Shenal Fonseka <146927943+Shenal-Fonseka@users.noreply.github.com>
2023-10-07 10:22:55 +0530

    Time: 54 ms (35.79%), Space: 42 MB (39.41%) - LeetHub

--- 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
+++ 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
@@ -1,12 +1,5 @@
-/**
- * @param {...(null|boolean|number|string|Array|Object)} args
- * @return {number}
- */
-var argumentsLength = function(...args) {
+const argumentsLength = (...args) => {
     return args.length
 	
 };
 
-/**
- * argumentsLength(1, 2, 3); // 3
- */
